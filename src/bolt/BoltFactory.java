edf9670f76ba2bc2963bd462308fa01879666aff
BiroMarton <rezoner9@gmail.com>
2017-02-26 10:27:57 +0100

    Part 5.1 completed

--- src/bolt/BoltFactory.java
+++ src/bolt/BoltFactory.java
@@ -5,8 +5,10 @@ import java.util.Date;
 import bolt.aruk.Tej;
 import bolt.aruk.tej.FeltartosTej;
 import bolt.aruk.tej.TartosTej;
+import bolt.aruk.Sajt;
+import bolt.aruk.Szappan;
 
-public abstract class TejFactory {
+public abstract class BoltFactory {
 	
 	public Tej ujTartosTej(long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom, long ar){
 		Tej tartos = new TartosTej(vonalKod, urtartalom, gyarto, szavatossagiIdo, zsirtartalom, ar);
@@ -19,5 +21,13 @@ public abstract class TejFactory {
 		return felTartos;
 		
 	}
+    public Sajt ujSajt(Long vonalKod, double suly, String gyarto, Date szavatossagiIdo, double zsirtartalom)
+    {
+        return new Sajt(vonalKod, suly, gyarto, szavatossagiIdo, zsirtartalom);
+    }
+    public Szappan ujSzappan(Long vonalkod, String gyarto, char mosohatas)
+    {
+        return new Szappan(vonalkod, gyarto, mosohatas);
+    }
 
 }
